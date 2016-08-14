e571bb761570e383831c4c633ff445af4d6fb253
Benji <benjipollock@gmail.com>
2016-08-14 12:56:59 +0300

    changed main

--- main.py
+++ main.py
@@ -134,10 +134,10 @@ def images(filename):
 
 
 def main():
-    if os.environ.get('APP_LOCATION') == 'heroku':
-        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        run(host='localhost', port=8080, debug=True)
+    #if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #else:
+     #   run(host='localhost', port=8080, debug=True)
 
 
 if __name__ == '__main__':
